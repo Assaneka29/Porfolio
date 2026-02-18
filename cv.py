0bfe4932ef51a1347fd88fd807e30cb8ed4a48f7
Assaneka29 <kaassane2909@gmail.com>
2026-02-18 13:23:08 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,27 @@
+import streamlit as st
+st.title("😎Porfolio")
+st.subheader("geomatique")
+st.text_input("technicien en geomatique,formé au CEDT le G15. je suis passionné par la geomatique,l'informatique,le sig et dans toute autres domaines concernant la geomatique.")
+st.subheader("competence")
+st.write("✅realisation d'une carte sig ou arcgis")
+st.write("✅realisation d'un plan 3D sur autocad")
+st.write("✅maitrise en cartographie")
+st.write("✅maitrise en topographie")
+
+            
+        
+        
+        
+            
+st.header("diplome")
+st.write("BTS:brevet technique superieur en cour")
+st.write("BAC")
+st.write("BFEM")
+
+with st.sidebar:
+    st.title("Assane ka")
+    st.subheader("🎓specialiste en Geomatique")
+    st.write("📞784727963")
+    st.write("📩Kaassane2909@gmail.com")
+    st.write("🚩DAKAR/SEN")
+
